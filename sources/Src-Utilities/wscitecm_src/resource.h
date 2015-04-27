f6828a35307f24127187375330b1f5441d4ae003
Roberto <--@--.-->
2015-04-27 13:49:57 +0200

    Aggiunto progetto per compilazione dll menu contestuale su gestore file

---
+++ sources/Src-Utilities/wscitecm_src/resource.h
@@ -0,0 +1,7 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDB_SCITE                               102
+#define IDS_ITEMSCITE                           103
+#define IDS_ITEMSCITEW                          104
