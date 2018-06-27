89a4a2f8dcbe3a6503bd063e2e155a6e7c1e1d04
Roberto <--@--.-->
2018-06-27 18:04:54 +0200

    Aggiunto supporto a lettere accentate unicode a procedura di inserimento codici html

--- sources/distro/luascr/acc2htm.lua
+++ sources/distro/luascr/acc2htm.lua
@@ -1,11 +1,11 @@
---[[
-Version : 2.1.4
+--[[# -*- coding: utf-8 -*-
+Version : 2.1.5
 Web     : http://www.redchar.net
 
 Questa procedura elimina tutte le lettere accentata da un file
 HTML sostituendole con i corrispondenti simboli
 
-Copyright (C) 2004-2009 Roberto Rossi 
+Copyright (C) 2004-2018 Roberto Rossi 
 *******************************************************************************
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
@@ -26,17 +26,20 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 do 
   require("luascr/rluawfx")
   
-  AccArray = {"è","é","à","ì","ò","ù"};
-  HtmlArray = {"&egrave;","&eacute;","&agrave;","&igrave;","&ograve;","&ugrave;"};
+  AccArray = {"è","é","à","ì","ò","ù", 
+              "Ã¨", "Ã©", "Ã ", "Ã¬", "Ã²", "Ã¹"};
+  HtmlArray = {"&egrave;","&eacute;","&agrave;","&igrave;","&ograve;","&ugrave;",
+               "&egrave;","&eacute;","&agrave;","&igrave;","&ograve;","&ugrave;"};
   
   ns = 0;
   
   function SubstAcc2Htm (id, val)
     local pos = true;
+    local pos2;
     while (pos) do
-      pos = editor:findtext(val)
+      pos,pos2 = editor:findtext(val)
       if (pos) then
-        editor:remove(pos, pos + 1);
+        editor:remove(pos, pos2);
         editor:insert(pos, HtmlArray[id]);
         ns = ns + 1;
       end
