6e003df07d52b45c0d9368208e728614cda2efe3
Roberto <--@--.-->
2018-07-04 20:43:40 +0200

    Generazione file HTML da markdown tramite F7. E' stato aggiunto il supporto per vari stili grafici ed è ora possibile aprire i file generati.

--- sources/distro/luascr/loadmd.lua
+++ sources/distro/luascr/loadmd.lua
@@ -1,11 +1,11 @@
 --[[
-Version : 2.0.0
+Version : 3.1.2
 Web     : http://www.redchar.net
 
 Questa procedura permette l'anteprima di un file markdown, convertendolo in html
 per poi mostrarlo all'interno del browser web del sistema
 
-Copyright (C) 2015-2018 Roberto Rossi 
+Copyright (C) 2015-2018 Roberto Rossi
 *******************************************************************************
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
@@ -27,78 +27,82 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 do
     require("luascr/rluawfx")
 
-        local markdown_header = [[
-        <!DOCTYPE html>
-        <html>
-         <head>
-         <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-         <meta charset="utf-8">
-         <STYLE type="text/css">
-        ]]    
-        local markdown_header2 = [[
-         </STYLE>
-         </head>
-         <body class="markdown">
-        ]]
-        
-        local markdown_footer = [[
-         </body>
-        </html>
-        ]]    
-    
+    local markdown_header = [[
+    <!DOCTYPE html>
+    <html>
+     <head>
+     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+     <meta charset="utf-8">
+     <STYLE type="text/css">
+    ]]
+    local markdown_header2 = [[
+     </STYLE>
+     </head>
+     <body class="markdown">
+    ]]
+
+    local markdown_footer = [[
+     </body>
+    </html>
+    ]]
+
     --ritorna il nome del file html di preview
     local function markdown_getHtmlName()
         --return os.getenv("TMP").."\\preview-"..props["FileName"]..".html"
         return props["FileDir"].."\\preview-"..props["FileNameExt"]..".html"
     end
-    
+
     local function get_Css_from_file(nomef)
       local result = ""
       local idf
-      
+
       idf = io.open(nomef, "r")
       if (idf) then
-        result = idf:read("*a")    
+        result = idf:read("*a")
         io.close(idf)
       end
 
       return result
     end
-    
+
     --aggiunge intestazione e piedi al file html specificato
-    local function markdown_modHtml(htmlFile)
+    local function markdown_modHtml(htmlFile, cssFile)
         local idf
         local text = ""
         local result = false
         local cssText = ""
         local headerTxt = ""
         
-        cssText = get_Css_from_file(props["SciteDefaultHome"]..
-                                        "\\luascr\\md\\default.css")
+        if (cssFile == false) then
+            cssText = get_Css_from_file(props["SciteDefaultHome"]..
+                                            "\\luascr\\md\\default.css")
+        else
+            cssText = get_Css_from_file(cssFile)
+        end
         
         headerTxt = markdown_header.."\n"..cssText.."\n"..markdown_header2
-        
+
         idf = io.open(htmlFile, "r")
         if (idf) then
             text = idf:read("*a") --lettura sorgente
             io.close(idf)
         end
-        
+
         --aggiunta intestazione
         text = headerTxt.."\n"..text
         --aggiunta chiusura
         text = text.."\n"..markdown_footer
-        
+
         idf = io.open(htmlFile, "w")--scrittura destination
         if (idf) then
           result = idf:write(text)
           io.close(idf)
           result = true
-        end     
-        
+        end
+
         return result
     end
-    
+
     --genera file bat con comando completo, ritorna il path del bat
     local function markdown_genBat(inPath)
         local batfile
@@ -112,30 +116,88 @@ do
         batfile = os.getenv("TMP")
         batfile = batfile.."\\sciteMarkdown.bat"
         idf = io.open(batfile, "w")
-        if (idf) then          
+        if (idf) then
           idf:write(cmd)
           io.close(idf)
         end
         return batfile
     end
----------------------------- Procedure principale ---------------------------- 
 
-    local function Main ()
-        local htmlFile = markdown_getHtmlName()
-        local htmlFileDest = markdown_getHtmlName() --props["FileDir"].."\\"..props["FileName"]..".html"
-        local copyFile = true
-        
-        rfx_exeCapture(markdown_genBat())
-        markdown_modHtml(htmlFile)
-        if (PUBLIC_optionScript == "RUN") then
-            --preview in browser
-            rwfx_ShellExecute(markdown_getHtmlName(),"")
-        elseif (PUBLIC_optionScript == "CREATE") then
-            print(_t(338)..htmlFileDest.._t(343))
-        end
-        PUBLIC_optionScript = ""
+
+---------------------------- Procedura principale ----------------------------
+    local function markdown_main()
+        local tblTemplates = {
+                              "github",
+                              "dark",
+                              "foghorn",
+                              "handwriting",
+                              "markdown",
+                              "metro_vibes",
+                              "metro_vibes_dark",
+                              "modern",
+                              "screen",
+                              "solarized_dark",
+                              "solarized_light"
+                             }
+        wcl_strip:init()
+        wcl_strip:addButtonClose()
+
+        wcl_strip:addLabel(nil, _t(416))
+        wcl_strip:addCombo("TVAL", nil)
+        wcl_strip:addButton("OKBTN",_t(418),buttonOk_click, true)
+        wcl_strip:addButton("OKBTN2",_t(419),buttonOk2_click, false)
+        wcl_strip:show()
+
+        wcl_strip:setList("TVAL", tblTemplates)
+        wcl_strip:setValue("TVAL", tblTemplates[1])
     end
 
-    Main()
+    function buttonOk_click(control, change)
+        markdown_ButtonClick(false)
+    end
+    
+    function buttonOk2_click(control, change)
+        markdown_ButtonClick(true)
+    end
+    
+    function markdown_ButtonClick(OpenHtml)
+        local htmlModel = wcl_strip:getValue("TVAL")
+        local htmlFile = markdown_getHtmlName()
+        local htmlFileDest = markdown_getHtmlName()
+        local cssFile = props["SciteDefaultHome"].."\\luascr\\md\\"..
+                        htmlModel..".css"
+        
+        if (rfx_fileExist(cssFile) == false) then
+            cssFile = props["SciteDefaultHome"].."\\luascr\\md\\default.css"
+        end
+        
+        print(_t(338)..htmlFileDest.._t(343))
+        rfx_exeCapture(markdown_genBat())        
+        markdown_modHtml(htmlFile, cssFile)
+        
+        if (OpenHtml) then
+            rwfx_ShellExecute(htmlFile,"")
+        end
+        
+        wcl_strip:close()
+    end
+    
+    local function main()
+        local htmlFile = markdown_getHtmlName()
+        local htmlFileDest = markdown_getHtmlName()
+
+        if (PUBLIC_optionScript == "RUN") then
+            --preview in browser
+            rfx_exeCapture(markdown_genBat())
+            markdown_modHtml(htmlFile, false)
+            rwfx_ShellExecute(markdown_getHtmlName(),"")
+            PUBLIC_optionScript = ""
+        elseif (PUBLIC_optionScript == "CREATE") then
+            PUBLIC_optionScript = ""
+            markdown_main()
+        end
+    end
+    
+    main()
 end
 
