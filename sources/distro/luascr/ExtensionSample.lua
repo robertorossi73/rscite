cf6382368ea5bd6bd5a4faf4e95af0547649cf05
Roberto <--@--.-->
2018-09-06 22:02:22 +0200

    modificato template per implementazioni funzioni personalizzate in F12 con nuove indicazioni e codifica utf-8

--- sources/distro/luascr/ExtensionSample.lua
+++ sources/distro/luascr/ExtensionSample.lua
@@ -1,20 +1,23 @@
 --[[Esempio Script LUA per RSciTE
+# -*- coding: utf-8 -*-
 
 Le Estensioni di RSciTE sono un metodo semplice e veloce per consentire 
 la creazione di nuove funzioni da utilizzare all'interno dell'editor.
 
-Per creare una nuova Estensione Ë necessario seguire alcune, semplici regole :
+Per creare una nuova Estensione √® necessario seguire alcune, semplici regole :
 
 1) Le Estensioni devono risiedere nella cartella "\RScite\Extensions\"
     presente all'interno della directory utente, nella sottocartella relativa
     ai dati delle applicazioni
     (es.: "C:\user\roberto\Dati applicazioni\RScite\Extensions\")
-2) Le Estensioni devono chiamarsi "scriptX.lua", dove X Ë un numero intero
+2) Le Estensioni devono chiamarsi "scriptX.lua", dove X √® un numero intero
     tra 1 e 50.
-3) Il titolo di ogni script, che comparir‡ nella finestra di F12, viene ricavato
-    dalla prima linea del file oppure, nel caso in cui questa sia vuota, 
-    dalla seconda linea (come succede in questo esempio).
-4) All'interno di una Estensione Ë possibile utilizzare qualsiasi istruzione
+3) Il titolo di ogni script, che comparir√† nella finestra di F12, viene ricavato
+    dalla prima linea del file
+4) La seconda linea del file contiene la codifica del file estesso, se √®
+    vuota viene usata una codifica standard in base al codepage del sistema
+    operativo. Si consigla di lasciare quella di default(utf-8)
+5) All'interno di una Estensione √® possibile utilizzare qualsiasi istruzione
     LUA supportata da SciTE/RSciTE, comprese le funzioni speciali della 
     distribuzione.
 
