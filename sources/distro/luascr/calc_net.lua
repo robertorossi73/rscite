6b2d1d3c25d615b30bd12fc4647ef18f89c73448
Roberto Rossi <--@--.-->
2021-07-04 14:55:05 +0200

    Aggiornata lancio calcolatrici. Introdotta calcolatrice di sistema e calcolatrice avanzata online.

--- sources/distro/luascr/calc_net.lua
+++ sources/distro/luascr/calc_net.lua
@@ -1,10 +1,10 @@
 --[[
 Version : 1.0.0
-Web     : http://www.redchar.net
+Web : http://www.redchar.net
 
-Questa procedura permette di lanciare la calcolatrice del sistema operativo
+Lancia la calcolatrice online fornita dal sito https://insect.sh/
 
-Copyright (C) 2004-2021 Roberto Rossi 
+Copyright (C) 2021-2022 Roberto Rossi
 *******************************************************************************
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
@@ -13,23 +13,21 @@ version 2.1 of the License, or (at your option) any later version.
 
 This library is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 Lesser General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public
 License along with this library; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 *******************************************************************************
 ]]
 
 do
-  --carica le funzioni speciali di RSciTE
   require("luascr/rluawfx")
-  
-  function main()
-    
-    rwfx_ShellExecute("calc.exe","")
+
+  local function main()
+    rwfx_ShellExecute("https://insect.sh/?q=%3F","")
   end
-  
+
   main()
-end --fine dello script
+end
