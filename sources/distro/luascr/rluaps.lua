a13029b82366a667e2506159b1b0ada6583c5480
Roberto Rossi <--@--.-->
2024-07-30 19:09:06 +0200

    Aggiornato modulo per gestione procedure Powershell

--- sources/distro/luascr/rluaps.lua
+++ sources/distro/luascr/rluaps.lua
@@ -1,5 +1,5 @@
 --[[ # -*- coding: utf-8 -*-
-Version : 1.0.3
+Version : 1.3.1
 Web     : http://www.redchar.net
 
 Funzioni di utilità per macro SciTE/Lua. Funzioni per la gestione di script
@@ -23,46 +23,38 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 *******************************************************************************
 ]]
 
---esegue script powershell
---  scriptTxt = testo script powershell
---  captureNoStop = cattura l'output ma non può ricevere imput utente
---  outFileName = salva l'output nel file specificato
-function rfx_powerShell(scriptTxt, captureNoStop, outFileName)
-  local psf
-  local loc_rfx_execPowerShell
-  local loc_rfx_genPowerShell
-  
-  function loc_rfx_genPowerShell(txtScript, utf8bom)
-    local psfile
-    local idf
-    local command
-    local exe = props["SciteDefaultHome"].."/tools/curl/curl.exe"
-    local outFolder = os.getenv("TMP")
-    
-    psfile = outFolder.."\\rscite ps.ps1"
-    idf = io.open(psfile, "w")
-    if (idf) then            
-      command = txtScript
-      if utf8bom then
-        idf:write("\xEF") -- tre caratteri per BOM utf-8
-        idf:write("\xBB")
-        idf:write("\xBF")
-        idf:write("chcp 65001")
+require("luascr/rluawfx")
+
+  -- powershell -ExecutionPolicy Bypass -File "$(FilePath)"
+  function rfx_execPowerShell(psfile, captureNoStop, outFileName, hideWindow, parms)
+    --genera file bat con comando completo, ritorna il path del bat
+    function genBat(comando)
+      local batfile
+      local idf
+      
+      batfile = os.getenv("TMP")
+      batfile = batfile.."\\rscitePS.bat"
+      idf = io.open(batfile, "w")
+      if (idf) then
+        idf:write(comando)
+        io.close(idf)
       end
-      --idf:write("\necho off")
-      idf:write("\ncls")
-      idf:write("\n"..command)            
-      io.close(idf)
-    else
-      psfile = ""
+      return batfile
     end
     
-    return psfile
-  end
-  
-  -- powershell -ExecutionPolicy Bypass -File "$(FilePath)"
-  function loc_rfx_execPowerShell(psfile, captureNoStop, outFileName)
-    local cmd = "powershell -ExecutionPolicy Bypass -WindowStyle Normal -Sta -OutputFormat TEXT -File \""..psfile.."\""
+    local winState = "Normal"
+    local cmd     
+    local res = ""
+    local ch
+    
+    if (hideWindow) then
+      winState = "hidden"
+    end
+    cmd = "powershell -ExecutionPolicy Bypass -WindowStyle "..winState.." -Sta -OutputFormat TEXT -File \""..psfile.."\""
+    
+    if (parms) then
+      cmd = cmd.." "..parms
+    end
     
     if (outFileName) then
       cmd = cmd.." >\""..outFileName.."\""
@@ -70,31 +62,63 @@ function rfx_powerShell(scriptTxt, captureNoStop, outFileName)
     
     if (captureNoStop) then
       -- ritorna risultato
-      return rfx_exeCapture(cmd)
+      res = rfx_exeCapture(cmd)
+      --rimuove ultimo ritorno a capo
+      res = rfx_RemoveReturnLine(res)
+      return res
     else
       -- ritorna true se il comando termina correttamente
-      return os.execute(cmd)
+      res = genBat("chcp 65001\n".."cls\n"..cmd)
+      return os.execute("\""..res.."\"")
     end  
   end
+
+  --esegue script powershell
+  --  scriptTxt = testo script powershell
+  --  captureNoStop = cattura l'output ma non può ricevere input utente
+  --  outFileName = salva l'output nel file specificato
+  function rfx_powerShell(scriptTxt, captureNoStop, outFileName)
+    local psf
+    local loc_rfx_genPowerShell
+    
+    function loc_rfx_genPowerShell(txtScript, utf8bom)
+      local psfile
+      local idf
+      local command
+      local outFolder = os.getenv("TMP")
+      
+      psfile = outFolder.."\\rscite.ps1"
+      idf = io.open(psfile, "w")
+      if (idf) then            
+        command = txtScript
+        if utf8bom then
+          idf:write("\xEF") -- tre caratteri per BOM utf-8
+          idf:write("\xBB")
+          idf:write("\xBF")
+          idf:write("chcp 65001")
+        end
+        --idf:write("\necho off")
+        idf:write("\ncls")
+        idf:write("\n"..command)            
+        io.close(idf)
+      else
+        psfile = ""
+      end
+      
+      return psfile
+    end
+    
+    psf = loc_rfx_genPowerShell(scriptTxt, true)
+    return rfx_execPowerShell(psf, captureNoStop, outFileName)
+  end
   
-  psf = loc_rfx_genPowerShell(scriptTxt, true)
-  return loc_rfx_execPowerShell(psf, captureNoStop, outFileName)
-end
+  
+-- TEST
 
---print( rfx_powerShell("dir c:\\temp\\unicode\\ci*.*\n[System.Console]::ReadKey()", true))
---print( rfx_powerShell("write-host \"Hello`nHello`ci®Ωo.txt\"\n[System.Console]::ReadKey()"))
---print( rfx_powerShell("write-host \"Hello`nHello`ci®Ωo.txt\""))
---hell("write-host \"Hello`nHello`ci®Ωo.txt\"\n[System.Console]::ReadKey()")
+-- ret = rfx_execPowerShell("c:\\Temp\\scite\\genguid.ps1", true, false, true)
+-- editor:ReplaceSel(ret)
 
--- scr = "$H = Get-Host\n$Win = $H.UI.RawUI.WindowSize\n$Win.Height = 10\n$Win.Width  = 10\n$H.UI.RawUI.Set_WindowSize($Win)"
--- res = rfx_powerShell(scr.."\n[System.Console]::ReadKey()")
--- if (res) then
---   print("ok")
--- else
---   print("error")
--- end
--- print("end")
+--rfx_execPowerShell("c:\\Temp\\scite\\loadCAD.ps1", false, false, false, "icad \"".. "c:\\Temp\\ci®Ωo.lsp" .."\"")
 
-
-
+-- END TEST
 
